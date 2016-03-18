4c5a38212ab295e792fc28a1be1c80e98a6ef953
David Davis <davisd50@gmail.com>
2016-03-17 22:05:42 -0400

    update for test layer

--- sparc/aws/__init__.py
+++ sparc/aws/__init__.py
@@ -2,12 +2,6 @@ from sparc.i18n import SparcMessageFactory
 MessageFactory = SparcMessageFactory('sparc.aws')
 
 
-# Configuration (this package only)
-from importlib import import_module
-from sparc.configuration.zcml import Configure as SparcConfigure
-def Configure():
-    SparcConfigure([import_module(__name__)])
-
 #Interface imports start here
 from interfaces import IAWSResource
 from interfaces import IBoto3Session
--- sparc/aws/tests/test_session.py
+++ sparc/aws/tests/test_session.py
@@ -1,13 +1,11 @@
 import unittest
-from doctest import DocTestSuite
-from doctest import DocFileSuite
+from sparc.testing.fixture import test_suite_mixin
 
-import sparc.aws
 
-def test_suite():
-    return unittest.TestSuite((
-        DocFileSuite('session.txt',
-                     package=sparc.aws),))
+class test_suite(test_suite_mixin):
+    package = 'sparc.aws'
+    module = 'session'
+
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
